f7ca454cb421670cc42924a7536164518c3fcbd6
Romain Rebecchi <romain.rebecchi@gmail.com>
2019-10-18 10:02:54 +0200

    package

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -6,19 +6,25 @@ using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
-    public Button but;
-    public GameObject canvas;
-    public GameObject button;
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
 
     public int action;
+
     // Start is called before the first frame update
     void Start()
     {
 
-        Button button = but.GetComponent<Button>();
+        Button button = button1.GetComponent<Button>();
         button.onClick.AddListener(Up);
+
        // Button button = but.GetComponent<Button>();
        // button.onClick.AddListener(Down);
+
        // Button button = but.GetComponent<Button>();
        // button.onClick.AddListener(Fire);
     }
@@ -26,33 +32,46 @@ public class ButtonScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!but.isActiveAndEnabled)
+        if (!button1.isActiveAndEnabled || !button2.isActiveAndEnabled || !button3.isActiveAndEnabled || !button4.isActiveAndEnabled || !button5.isActiveAndEnabled || !button6.isActiveAndEnabled)
         {
             action = 0;
+            Debug.Log("Action 0");
         }
     }
-    void Up()
+
+    public void Up()
     {
         action = 1;
+        Debug.Log("Action 1");
     }
-    void Down()
+
+    public void Down()
     {
         action = 2;
+        Debug.Log("Action 2");
     }
-    void Fire()
+
+    public void Fire()
     {
         action = 3;
+        Debug.Log("Action 3");
     }
-    void Up2()
+
+    public void Up2()
     {
         action = 1;
+        Debug.Log("Action 1");
     }
-    void Down2()
+
+    public void Down2()
     {
         action = 2;
+        Debug.Log("Action 2");
     }
-    void Fire2()
+
+    public void Fire2()
     {
         action = 3;
+        Debug.Log("Action 3");
     }
 }
